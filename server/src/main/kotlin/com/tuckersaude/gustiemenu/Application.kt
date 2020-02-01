a13bb7c88672540e8352e7575f0564ee2744cdf6
Tucker Saude <tuckersaude@gmail.com>
2020-02-01 12:40:51 -0600

    fresh micronaut project

---
+++ server/src/main/kotlin/com/tuckersaude/gustiemenu/Application.kt
@@ -0,0 +1,14 @@
+package com.tuckersaude.gustiemenu
+
+import io.micronaut.runtime.Micronaut
+
+object Application {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Micronaut.build()
+                .packages("com.tuckersaude.gustiemenu")
+                .mainClass(Application.javaClass)
+                .start()
+    }
+}
